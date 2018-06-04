6b8a31ad4417b11782a9d19c20ce1831be0baae6
John Newman <newmanj903@gmail.com>
2018-06-04 09:40:03 +0100

    adding numbers

--- chapter2-DataTypes/exercises/Numbers.java
+++ chapter2-DataTypes/exercises/Numbers.java
@@ -1,12 +1,12 @@
 public class Numbers {
   public static void main(String[] args) {
     // declare an integer variable with a value
-
+    int numberFive = 5;
     // declare a double variable with a value
-
+    double eightPoint = 8.2;
     // store the result of adding the two together in a variable
-
+    double adding = numberFive + eightPoint;
     // print it
-
+    System.out.println(adding);
   }
 }
--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,12 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String first_name = "John";
     // declare the second name
-
+    String second_name = "Jane";
     // create your message
-
+    String message = String.format( "%s and %s are learning Java!",first_name, second_name);
     // print your message!
-
+    System.out.println(message);
   }
 }
